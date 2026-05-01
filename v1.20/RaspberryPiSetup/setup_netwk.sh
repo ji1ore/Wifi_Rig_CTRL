db591bec52e39aa466ced5966c06858bfb964a9c
ji1ore <jiro.ueno0213@gmail.com>
2026-05-02 07:52:49 +0900

    Delete v1.20 directory

--- v1.20/RaspberryPiSetup/setup_netwk.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee /etc/sysctl.d/99-disable-ipv6.conf
-echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.d/99-disable-ipv6.conf
